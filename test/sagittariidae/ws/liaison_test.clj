cdb05f5a37952b40d518937fd4ff8b4c4f99034f
Marc Daya <marc@sinistral.net>
2016-10-04 15:27:48 -0600

    Refactor `liaison` tests.
    The superfluous Vars have been been removed in favour of direct
    references to the private fns in the namespace.  Using the Vars saved
    only 4 chars and added an unecessary level of indirection.
    
    `find-by-name` has been made more specific to retrieve only the
    `obfuscated-id` for a resource, as that is the pattern when referring to
    resources.

--- test/sagittariidae/ws/liaison_test.clj
+++ test/sagittariidae/ws/liaison_test.clj
@@ -7,29 +7,23 @@
             [sagittariidae.ws.db-aux  :refer [mk-db speculate]]
             [sagittariidae.ws.liaison :as    <>]))
 
-(def extern-name #'<>/extern-name)
-
 (deftest test:extern-name
   (loop [x ["Foo Bar" "Foo   Bar" "Foo#Bar" "Foo###Bar" "foo|bar" "ns$foo-bar"]]
     (when x
       (do
-        (is (= "foo-bar" (extern-name (first x))))
+        (is (= "foo-bar" (#'<>/extern-name (first x))))
         (recur (next x)))))
-  (is (= "f-o-o" (extern-name "f o o"))))
-
-(def untype-attrs #'<>/untype-attrs)
+  (is (= "f-o-o" (#'<>/extern-name "f o o"))))
 
 (deftest test:untype-attrs
   (is (= [[:foo 0] [:bar 1]]
-         (untype-attrs [[:ns0/foo 0] [:ns1/bar 1]]))))
-
-(def extern-id #'<>/extern-id)
+         (#'<>/untype-attrs [[:ns0/foo 0] [:ns1/bar 1]]))))
 
 (deftest test:extern-id
   (is (= {:id "f00-bar" :name "Bar"}
-         (extern-id {:id 0 :obfuscated-id "f00" :name "Bar"})))
+         (#'<>/extern-id {:id 0 :obfuscated-id "f00" :name "Bar"})))
   (is (= {:id "f00"}
-         (extern-id {:id 0 :obfuscated-id "f00"}))))
+         (#'<>/extern-id {:id 0 :obfuscated-id "f00"}))))
 
 (def extern-resource-entity #'<>/extern-resource-entity)
 
@@ -42,15 +36,11 @@
                                   :thing/attr0         "nil"
                                   :other-thing/attr1   "one"}))))
 
-(def tx-data:add-resource #'<>/tx-data:add-resource)
-(def tx-data:add-project #'<>/tx-data:add-project)
-(def tx-data:add-method #'<>/tx-data:add-method)
-
 (deftest test:get-projects
   (let [db (as-> (mk-db) db
-             (speculate db (tx-data:add-project db "p1" ""))
-             (speculate db (tx-data:add-project db "p2" ""))
-             (speculate db (tx-data:add-resource
+             (speculate db (#'<>/tx-data:add-project db "p1" ""))
+             (speculate db (#'<>/tx-data:add-project db "p2" ""))
+             (speculate db (#'<>/tx-data:add-resource
                             :res.type/method {:project/name "m1"})))]
                                         ; This isn't a legal construction, but
                                         ; it serves to illustrate that we find
@@ -62,33 +52,33 @@
 
 (deftest test:get-methods
   (let [db (as-> (mk-db) db
-             (speculate db (tx-data:add-resource
+             (speculate db (#'<>/tx-data:add-resource
                             :res.type/project {:method/name "p1"}))
                                         ; This isn't a legal construction, but
                                         ; it serves to illustrate that we find
                                         ; methods based on their "type", and
                                         ; not by other assigned attributes.
-             (speculate db (tx-data:add-method db "m1" ""))
-             (speculate db (tx-data:add-method db "m2" "")))]
+             (speculate db (#'<>/tx-data:add-method db "m1" ""))
+             (speculate db (#'<>/tx-data:add-method db "m2" "")))]
     (is (= [{:name "m1", :id "XZOQ0-m1" :description ""}
             {:name "m2", :id "Xd9k2-m2" :description ""}]
            (<>/get-methods db)))))
 
-(def tx-data:add-sample #'<>/tx-data:add-sample)
-
-(defn find-by-name
+(defn name->obid
   [db t n]
-  (let [a (keyword (clojure.string/join "/" [(name t) "name"]))]
-    (ffirst (d/q '[:find  (pull ?e [*])
-                   :in    $ ?a ?n
-                   :where [?e ?a ?n]]
-                 db a n))))
+  (let [type-attr (fn [t a] (keyword (clojure.string/join "/" [(name t) a])))
+        name-attr (type-attr t "name")
+        obid-attr (type-attr t "obfuscated-id")]
+    (obid-attr (ffirst (d/q '[:find  (pull ?e [*])
+                              :in    $ ?a ?n
+                              :where [?e ?a ?n]]
+                            db name-attr n)))))
 
 (deftest test:add-sample
   (testing "one new sample"
-    (let [db (as-> (mk-db) db (speculate db (tx-data:add-project db "p1" ".*")))
-          pn (:project/obfuscated-id (find-by-name db :project "p1"))
-          db (speculate db (tx-data:add-sample db pn "s1"))]
+    (let [db (as-> (mk-db) db (speculate db (#'<>/tx-data:add-project db "p1" ".*")))
+          pn (name->obid db :project "p1")
+          db (speculate db (#'<>/tx-data:add-sample db pn "s1"))]
       (is (= [(s/join "$" [pn "s1"])]
              (map :sample/name
                   (d/q '[:find  [(pull ?s [:sample/name]) ...]
@@ -97,11 +87,11 @@
                        db))))))
   (testing "two new samples"
     (let [db (as-> (mk-db) db
-               (speculate db (tx-data:add-project db "p1" ".*")))
-          pn (:project/obfuscated-id (find-by-name db :project "p1"))
+               (speculate db (#'<>/tx-data:add-project db "p1" ".*")))
+          pn (name->obid db :project "p1")
           db (as-> db db
-               (speculate db (tx-data:add-sample db pn "s1"))
-               (speculate db (tx-data:add-sample db pn "s2")))]
+               (speculate db (#'<>/tx-data:add-sample db pn "s1"))
+               (speculate db (#'<>/tx-data:add-sample db pn "s2")))]
       (let [samples (d/q '[:find  [(pull ?s [*]) ...]
                            :where [?p :project/sample ?s]
                            [?p :project/name "p1"]]
@@ -112,22 +102,22 @@
                (map :sample/name samples))))))
   (testing "duplicate sample"
     (let [db (as-> (mk-db) db
-               (speculate db (tx-data:add-project db "p1" ".*")))
-          pn (:project/obfuscated-id (find-by-name db :project "p1"))]
+               (speculate db (#'<>/tx-data:add-project db "p1" ".*")))
+          pn (name->obid db :project "p1")]
       (is (thrown-with-msg? java.lang.IllegalStateException
               #"Unique conflict"
             (as-> db db
-              (speculate db (tx-data:add-sample db pn "s1"))
-              (speculate db (tx-data:add-sample db pn "s1")))))))
+              (speculate db (#'<>/tx-data:add-sample db pn "s1"))
+              (speculate db (#'<>/tx-data:add-sample db pn "s1")))))))
   (testing "two new samples in different projects"
     (let [db (as-> (mk-db) db
-               (speculate db (tx-data:add-project db "p1" ".*"))
-               (speculate db (tx-data:add-project db "p2" ".*")))
-          p1 (:project/obfuscated-id (find-by-name db :project "p1"))
-          p2 (:project/obfuscated-id (find-by-name db :project "p2"))
+               (speculate db (#'<>/tx-data:add-project db "p1" ".*"))
+               (speculate db (#'<>/tx-data:add-project db "p2" ".*")))
+          p1 (name->obid db :project "p1")
+          p2 (name->obid db :project "p2")
           db (as-> db db
-               (speculate db (tx-data:add-sample db p1 "s1"))
-               (speculate db (tx-data:add-sample db p2 "s1")))]
+               (speculate db (#'<>/tx-data:add-sample db p1 "s1"))
+               (speculate db (#'<>/tx-data:add-sample db p2 "s1")))]
       (testing "sample IDs are globally unique (i.e. across all samples)"
         (is (= [1 2]
                (sort (map :sample/id (d/q '[:find  [(pull ?s [*]) ...]
@@ -146,22 +136,21 @@
     (is (thrown-with-msg? clojure.lang.ExceptionInfo
             #"requested resource Project:none could not be found"
           (let [db (mk-db)]
-            (speculate db (tx-data:add-sample db "none" "s1"))))))
+            (speculate db (#'<>/tx-data:add-sample db "none" "s1"))))))
   (testing "sample name does not match pattern"
     (is (thrown-with-msg? clojure.lang.ExceptionInfo
             #"does not conform to .* pattern"
           (let [db (as-> (mk-db) db
-                     (speculate db (tx-data:add-project db "p1" #"s-\d")))
-                pn (:project/obfuscated-id (find-by-name db :project "p1"))]
-            (speculate db (tx-data:add-sample db pn "s-a")))))))
+                     (speculate db (#'<>/tx-data:add-project db "p1" #"s-\d")))
+                pn (name->obid db :project "p1")]
+            (speculate db (#'<>/tx-data:add-sample db pn "s-a")))))))
 
 (deftest test:get-sample
-  (let [db (as-> (mk-db) db (speculate db (tx-data:add-project db "p1" ".*")))
-        pi (:project/obfuscated-id (find-by-name db :project "p1"))
-        db (speculate db (tx-data:add-sample db pi "s1"))]
+  (let [db (as-> (mk-db) db (speculate db (#'<>/tx-data:add-project db "p1" ".*")))
+        pi (name->obid db :project "p1")
+        db (speculate db (#'<>/tx-data:add-sample db pi "s1"))]
     (testing "one sample"
       (is (= {:name "s1", :id "OQn6Q-s1"}
-             (<>/get-sample db pi (:sample/obfuscated-id
-                                   (find-by-name db :sample (#'<>/mk-sample-name pi "s1")))))))
+             (<>/get-sample db pi (name->obid db :sample (#'<>/mk-sample-name pi "s1"))))))
     (testing "no sample"
       (is (nil? (<>/get-sample db pi "..."))))))
